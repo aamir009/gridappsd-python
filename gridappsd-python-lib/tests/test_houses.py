9313d30c32b6c61d367a33d25993bc52c6e0fd78
aamir009 <engr.aamir87@yahoo.com>
2024-07-13 03:26:50 +0500

    Add files via upload

---
+++ gridappsd-python-lib/tests/test_houses.py
@@ -0,0 +1,12 @@
+#
+# def test_multiple_calls_gets_same_houses_obj(gridappsd_client):
+#     gapps = gridappsd_client
+#     houses = gapps.get_houses()
+#     id_of_houses = id(houses)
+#     houses_again = gapps.get_houses()
+#     assert id_of_houses == id(houses_again)
+
+# def test_can_get_transactive_houses(gridappsd_client):
+#
+#    houses = gridappsd_client.get_houses().get_houses_for_feeder('_503D6E20-F499-4CC7-8051-971E23D0BF79')
+#    assert houses
